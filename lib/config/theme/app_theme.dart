ddffc787a4d79733d5987322a15f86b2b0d400e5
Ricardo Bastardo <rbrodriguez.siaca@gmail.com>
2025-05-02 16:48:54 -0400

    first commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+
+  ThemeData getTheme() {
+    const seedColor = Colors.deepPurple;
+
+    return ThemeData(
+      colorSchemeSeed: seedColor,
+      listTileTheme: const ListTileThemeData(
+        iconColor: seedColor
+      ),
+    );
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:forms_app/config/router/app_router.dart';
+import 'package:forms_app/config/theme/app_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Material App',
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+    );
+  }
+}
